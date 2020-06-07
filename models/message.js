397682f319e7078b8ca0ef14d168cc0a6ddf042c
ahffron <ahffron22@gmail.com>
2020-06-06 23:32:50 -0600

    almost working message box

---
+++ models/message.js
@@ -0,0 +1,33 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var UserMessage = new Schema({
+    email: {
+        type: String,
+        trim: true,
+        match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+        required: "String is Required"
+    },
+    subject: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    message: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+
+});
+
+
+var Message = mongoose.model("Message", UserMessage);
+
+
+module.exports = Message;
--- server.js
+++ server.js
@@ -3,23 +3,37 @@ var mongojs = require("mongojs");
 var axios = require("axios");
 var cheerio = require("cheerio");
 var mongoose = require("mongoose");
-var app = express();
 var logger = require("morgan");
+
+var PORT = 3000;
+
+// Require all models
+var db = require("./models/Message");
+
+var app = express();
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-var databaseUrl = "userMessagesDB";
-var collections = ["messages"];
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/userMessagesDB", { useNewUrlParser: true });
 
-var MONGODB_URI =
-    process.env.MONGODB_URI || "mongodb://localhost/userMessagesDB";
+var Message = require("./models/Message")
 
-mongoose.connect(MONGODB_URI);
-
-var db = mongojs(databaseUrl, collections);
+app.get("/savedMessages", function (req, res) {
+    // Find all Notes
+    db.Message.find({})
+        .then(function (dbMessage) {
+            // If all Notes are successfully found, send them back to the client
+            res.json(dbMessage);
+        })
+        .catch(function (err) {
+            // If an error occurs, send the error back to the client
+            res.json(err);
+        });
+});
 
 app.listen(3000, function () {
     console.log("App running on port 3000!");
